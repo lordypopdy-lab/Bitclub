54e41eef86404e62f76ddf7819715b64efb5e271
James Issac <jamesissac@Jamess-MacBook-Pro.local>
2026-04-09 10:29:01 -0700

    safe

--- client/src/pages/models/DetailChartModal.jsx
+++ client/src/pages/models/DetailChartModal.jsx
@@ -150,7 +150,7 @@ export const DetailChartModal = ({ details, isOpen, onClose }) => {
           {/* 🔥 CHART */}
           <div style={{ marginTop: "20px" }}>
             <ModalSparkline
-              width={337}
+              width={330}
               height={120}
               priceChangePercent={change}
               data={chartData}
