f5e5caf2001b5b7ffdaf4537697bb01b2716eb09
James Issac <jamesissac@Jamess-MacBook-Pro.local>
2026-04-29 01:44:51 -0700

    safe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -82,6 +82,7 @@ import { Preloader } from "../src/pages/utils/Properties";
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 axios.defaults.withCredentials = true;
 
+//
 function App() {
   useEffect(() => {
     Preloader();
